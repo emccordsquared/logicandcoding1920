25a3a0c0297b1bb0b2bc98d7dcaf3e78b7f305b9
emccordsquared <54411986+emccordsquared@users.noreply.github.com>
2019-09-03 09:38:00 -0700

    Add files via upload

---
+++ Completenomial.java
@@ -0,0 +1,19 @@
+/**
+ * Skeleton program for Week 4 Lab on Operators and Conditional Branching.
+ *
+ * @author Roy Chancellor
+ * @version 2/6/2019
+ */
+public class Completenomial
+{
+    public static void main( String[] args ) {
+        //variable declarations
+        
+        
+        //Polynomial computation
+        
+        
+        //Output the result
+        System.out.println("");
+    }
+}
---
+++ PartialTempConverter.java
@@ -0,0 +1,28 @@
+/**
+ * Skeleton of a program that convertes temperature from Fahrenheit to Celsius
+ *
+ * @author Roy Chancellor
+ * @version 2/6/2019
+ */
+
+import java.util.Scanner;  //used to get user input
+
+public class PartialTempConverter
+{
+    public static void main( String[] args) {
+        //variable declarations
+        double f;
+        double c;
+        //enable user input
+        Scanner keys = new Scanner(System.in);
+        System.out.println("Enter the temperature in degrees Fahrenheit:  ");
+        f = keys.nextDouble();  //converts keyboard input into a floating point number
+        keys.close();  //close the keyboard scanner
+        
+        //temperature conversion
+       
+        
+        //output to user
+        System.out.println();
+    }
+}
