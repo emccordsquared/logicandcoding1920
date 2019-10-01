409bda0ee1b6763b9ea6cbb618bf600574fcebd0
emccordsquared <54411986+emccordsquared@users.noreply.github.com>
2019-10-01 13:25:49 -0700

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,24 @@
+public class ForLoops
+{
+    public static void main (String args[] ) {
+        /*int cube;
+        cube = 1;
+        for (int i =1;i<=10; i++) {
+            cube = i*i*i;
+            System.out.println(i + "\t" + cube);
+        }*/
+        
+        /*int diff;
+        for(int i = 99; i>=1; i = i - 2) {
+            diff = i;
+            System.out.println(diff);
+        } */
+        
+        /*String mySentence = "The quick brown fox jumped over the lazy dog";
+   
+        for (int i = 0; i < mySentence.length(); i ++) {
+            System.out.println(mySentence.charAt(i));
+        } */
+    
+    }
+}
