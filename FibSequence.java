d16a1b74e33d6ef9899f936e79600cd8662151c6
emccordsquared <54411986+emccordsquared@users.noreply.github.com>
2020-02-03 07:22:46 -0700

    Update FibSequence.java

--- FibSequence.java
+++ FibSequence.java
@@ -5,16 +5,14 @@ public class FibSequence{
 public static void main(String args[]){
 /*prompt the user for a number here. 
  *then, pass that number as an argument to the sequence method (ie call the method).
- *finally, print the result to the console
+ *finally, print the result to the console.
  */
 }
 
 public static long sequence(int j){
-    long p =j;
 /*build the method here from the recursive formula.
  *what needs to be given? why do you think the return type is long rather than int?
  */
-    return p;
 }
 
 
