1b747880e94bd5e60fa99f5db66125fcc79adfb0
emccordsquared <54411986+emccordsquared@users.noreply.github.com>
2020-02-13 14:55:04 -0700

    Update Pally.java

--- Pally.java
+++ Pally.java
@@ -6,32 +6,14 @@ public class Pally{
         Scanner keys = new Scanner(System.in);
         System.out.println("Enter your palindrome-ish phrase with no spaces.");
         String myPhrase = keys.nextLine();
-        int start = 0;
-        int length = myPhrase.length()-1;
         
-        boolean result = palindrome(myPhrase, length, start);
+        
+        boolean result = palindrome(//your variables go here);
         System.out.println("Is it a palindrome? " + result);
     }
     
-    public static boolean palindrome(String s, int n, int i){
-        /*if (s.charAt(i) != s.charAt(n-1)){
-            return false;
-        }
-        if (i >= (n-1)  ){
-            return true;
-        }
-        else {
-           return palindrome(s, n-1, i+1);
-        }*/
-        if(s.charAt(i) != s.charAt(n)){
-            return false;
-        }
-        else{
-            if(i<=n-1){
-            return palindrome(s, n-1, i+1);
-            
-        }
-        return true;
+    public static boolean palindrome(//your arguments go here){
+       //your method goes here
     }
     }
 }
